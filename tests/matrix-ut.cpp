c32b707bae69e58d29c42532034b0023710b7f46
damixiorek <damixiorek@gmail.com>
2021-01-31 13:53:46 +0100

    add matrix tests file

---
+++ tests/matrix-ut.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+class MatrixTest : public ::testing::Test {
+public:
+};
+
+TEST_F(MatrixTest, ShouldCreateMatrixWithSpecifiedNumsOfRowsAndColumns)
+{
+    EXPECT_EQ(1,1);
+}

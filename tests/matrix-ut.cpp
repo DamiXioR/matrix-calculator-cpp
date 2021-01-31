acf9f7e8eee05ce00c3131ba28977a7bfa850b6f
damixiorek <damixiorek@gmail.com>
2021-01-31 14:36:41 +0100

    add matrix tests for getRows and getColumns

--- tests/matrix-ut.cpp
+++ tests/matrix-ut.cpp
@@ -1,10 +1,49 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+#include "matrix.hpp"
+
 class MatrixTest : public ::testing::Test {
 public:
+    ~MatrixTest()
+    {
+        delete testMatrix;
+    }
+
+    Matrix* testMatrix = nullptr;
+    const unsigned testRows5 = 5;
+    const unsigned testColumns7 = 7;
+    const unsigned testRows10 = 10;
+    const unsigned testColumns1 = 1;
 };
+
+// IMPORTANT! testMatrix freed in MatrixTest dtor, you haven't delete it at the end of every test case
+// except when you alloc new memory more than one time in one test case
 
 TEST_F(MatrixTest, ShouldCreateMatrixWithSpecifiedNumsOfRowsAndColumns)
 {
-    EXPECT_EQ(1,1);
+    testMatrix = new Matrix(testRows5, testColumns7);
+    EXPECT_EQ(testMatrix->getMatrix()->size(), testRows5);
+    for (auto& everyRow : *testMatrix->getMatrix()) {
+        EXPECT_EQ(everyRow.size(), testColumns7);
+    }
+    delete testMatrix;
+
+    testMatrix = new Matrix(testRows10, testColumns1);
+    EXPECT_EQ(testMatrix->getMatrix()->size(), testRows10);
+    for (auto& everyRow : *testMatrix->getMatrix()) {
+        EXPECT_EQ(everyRow.size(), testColumns1);
+    }
+}
+
+TEST_F(MatrixTest, ShouldReturnValueOfRowsOrColumns)
+{
+    testMatrix = new Matrix(testRows5, testColumns7);
+    EXPECT_EQ(testMatrix->getRows(), testRows5);
+    EXPECT_EQ(testMatrix->getColumns(), testColumns7);
+    delete testMatrix;
+
+    testMatrix = new Matrix(testRows10, testColumns1);
+    EXPECT_EQ(testMatrix->getRows(), testRows10);
+    EXPECT_EQ(testMatrix->getColumns(), testColumns1);
 }

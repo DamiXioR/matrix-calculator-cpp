f3b4b8ca3684bcd6765b7b001c9831ae32a8380e
damixiorek <damixiorek@gmail.com>
2021-02-02 10:18:51 +0100

    add matrixManager files, mod CMakeLists

---
+++ src/matrixManager.cpp
@@ -0,0 +1,1 @@
+#include "matrixManager.hpp"
---
+++ tests/matrixManager-ut.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "matrixManager.hpp"
+
+class MatrixManagerTest : public ::testing::Test {
+public:
+};
+
+TEST_F(MatrixManagerTest, MatrixManagerTest)
+{
+    EXPECT_EQ(1, 1);
+}

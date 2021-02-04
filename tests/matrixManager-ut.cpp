397c1bb967739d289248770af32efe4f58d7b1cc
damixiorek <damixiorek@gmail.com>
2021-02-04 21:12:57 +0100

    add tests to MatrixManager class for addTwoMatrixes method

--- tests/matrixManager-ut.cpp
+++ tests/matrixManager-ut.cpp
@@ -1,13 +1,31 @@
 #include <gtest/gtest.h>
 
-#include "matrixManager.hpp"
 #include "matrix.hpp"
+#include "matrixManager.hpp"
 
 class MatrixManagerTest : public ::testing::Test {
 public:
+    MatrixManager* matrixManager = nullptr;
+    Matrix* firstMatrix = nullptr;
+    Matrix* secondMatrix = nullptr;
+
+    ~MatrixManagerTest()
+    {
+    }
 };
 
-TEST_F(MatrixManagerTest, MatrixManagerTest)
+TEST_F(MatrixManagerTest, ShouldAddNewMatrixToMatrixHolderWhenKeyNotExists)
 {
-    EXPECT_EQ(1, 1);
+    matrixManager = new MatrixManager();
+    firstMatrix = new Matrix(3, 2);
+    secondMatrix = new Matrix(3, 2);
+    char a = 'a';
+    char b = 'b';
+
+    EXPECT_TRUE(matrixManager->addNewMatrix(a, *firstMatrix));
+    EXPECT_TRUE(matrixManager->addNewMatrix(b, *secondMatrix));
+
+    delete matrixManager;
+    delete firstMatrix;
+    delete secondMatrix;
 }

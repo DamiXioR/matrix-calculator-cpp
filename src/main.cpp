7ebf3a7b1de39ddd47d2e9859bde7c99bf8a588c
damixiorek <damixiorek@gmail.com>
2021-02-04 21:13:52 +0100

    mod main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
 #include "allHeaders.hpp"
 
+#include <iostream>
+
 int main(){
 
-    Matrix matrix(5,7);
-    matrix.printMatrix();
-    matrix.setMatrixValues();
-    matrix.printMatrix();
-    
-    MatrixManager mManager = MatrixManager{};
-    mManager.addTwoMatrixes('a','b');
+    Matrix* matrix = new Matrix(2,3);
+    matrix->setMatrixValues();
+    matrix->printMatrix();
 
+    Matrix* matrixB = new Matrix(2,3);
+    matrixB->setMatrixValues();
+    matrixB->printMatrix();
+    
+    MatrixManager* mManager = new MatrixManager();
+    mManager->addNewMatrix('a', *matrix);
+    mManager->addNewMatrix('b',*matrixB);
+
+    Matrix matrixC = mManager->addTwoMatrixes('a','b');
+    matrixC.printMatrix();
+
+    delete mManager;
+    delete matrix;
+    delete matrixB;
     
     return 0;
 }

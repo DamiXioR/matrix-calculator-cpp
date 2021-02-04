cf4dfe36dc7200e8adecfb7c97edaf9dcabb6d08
damixiorek <damixiorek@gmail.com>
2021-02-04 18:00:20 +0100

    create method to matrixes to matrixManager class

--- headers/matrixManager.hpp
+++ headers/matrixManager.hpp
@@ -1,4 +1,18 @@
 #pragma once
 
+#include <map>
+#include <vector>
+#include "matrix.hpp"
+
 class MatrixManager {
+public:
+
+    Matrix addTwoMatrixes(char firstMatrixKey, char secondMatrixKey);
+    std::map<char, Matrix> getMatrixHolder() const
+    {
+        return matrixHolder_;
+    }
+
+private:
+    std::map<char, Matrix> matrixHolder_;
 };
--- src/matrixManager.cpp
+++ src/matrixManager.cpp
@@ -1,1 +1,37 @@
 #include "matrixManager.hpp"
+
+#include <algorithm>
+
+Matrix MatrixManager::addTwoMatrixes(char firstMatrixKey, char secondMatrixKey)
+{
+    Matrix result = Matrix();
+    if (getMatrixHolder().size() > 1) {
+        auto firstMatrixIterator = getMatrixHolder().find(firstMatrixKey);
+        auto secondMatrixIterator = getMatrixHolder().find(secondMatrixKey);
+
+        if (firstMatrixIterator != getMatrixHolder().end() && secondMatrixIterator != getMatrixHolder().end()) {
+            std::vector<std::vector<double>> firstMatrix = *(firstMatrixIterator->second.getMatrix());
+            std::vector<std::vector<double>> secondMatrix = *(secondMatrixIterator->second.getMatrix());
+
+            bool areRowsEqual = firstMatrixIterator->second.getRows() == secondMatrixIterator->second.getRows();
+            bool areColumnsEqual = firstMatrixIterator->second.getColumns() == secondMatrixIterator->second.getColumns();
+
+            if (areRowsEqual && areColumnsEqual) {
+                std::vector<std::vector<double>> thirdMatrix = *(result.getMatrix());
+
+                std::transform(firstMatrix.begin(), firstMatrix.end(), secondMatrix.begin(), std::back_inserter(thirdMatrix),
+                               [](std::vector<double> firstVector, std::vector<double> secondVector) {
+                                   std::vector<double> everyRow;
+
+                                   std::transform(firstVector.begin(), firstVector.end(), secondVector.begin(), std::back_inserter(everyRow),
+                                                  [](double firstNum, double secondNum) {
+                                                      double sumOfTwoRows = firstNum + secondNum;
+                                                      return sumOfTwoRows;
+                                                  });
+                                   return everyRow;
+                               });
+            }
+        }
+    }
+    return result;
+}

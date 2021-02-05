3a1f606a462ea88e327466c3492a92876998d5c8
damixiorek <damixiorek@gmail.com>
2021-02-05 19:03:58 +0100

    refactorized matrixManager tests

--- tests/matrixManager-ut.cpp
+++ tests/matrixManager-ut.cpp
@@ -8,9 +8,35 @@ public:
     MatrixManager* matrixManager = nullptr;
     Matrix* firstMatrix = nullptr;
     Matrix* secondMatrix = nullptr;
+    Matrix* thirdMatrix = nullptr;
+
+    std::vector<std::vector<double>>* aMatrixContent = new std::vector<std::vector<double>>{
+        {1, 2, 3},
+        {10, 6, 1},
+        {2, 1, 2},
+    };
+
+    std::vector<std::vector<double>>* bMatrixContent = new std::vector<std::vector<double>>{
+        {4, 4, 4},
+        {-15, 20, 11},
+        {-2, -1, -1},
+    };
+
+    std::vector<std::vector<double>>* cMatrixContent = new std::vector<std::vector<double>>{
+        {5, 6, 7},
+        {-5, 26, 12},
+        {0, 2, 1},
+    };
 
     ~MatrixManagerTest()
     {
+        delete matrixManager;
+        delete firstMatrix;
+        delete secondMatrix;
+        delete thirdMatrix;
+        delete aMatrixContent;
+        delete bMatrixContent;
+        delete cMatrixContent;
     }
 };
 
@@ -24,8 +50,27 @@ TEST_F(MatrixManagerTest, ShouldAddNewMatrixToMatrixHolderWhenKeyNotExists)
 
     EXPECT_TRUE(matrixManager->addNewMatrix(a, *firstMatrix));
     EXPECT_TRUE(matrixManager->addNewMatrix(b, *secondMatrix));
+}
 
-    delete matrixManager;
-    delete firstMatrix;
-    delete secondMatrix;
+TEST_F(MatrixManagerTest, ShouldAddTwoMatrixesAndReturnMatrixConsistSum)
+{
+    matrixManager = new MatrixManager();
+    firstMatrix = new Matrix(3, 3);
+    secondMatrix = new Matrix(3, 3);
+    thirdMatrix = new Matrix();
+    char a = 'a';
+    char b = 'b';
+
+    [[maybe_unused]] auto extractedfirstMatrix = firstMatrix->getMatrix();
+    [[maybe_unused]] auto extractedsecondMatrix = secondMatrix->getMatrix();
+
+    extractedfirstMatrix = aMatrixContent;
+    extractedsecondMatrix = bMatrixContent;
+
+    delete thirdMatrix;
+    thirdMatrix = matrixManager->addTwoMatrixes(a, b);
+    auto extractedThirdMatrix = thirdMatrix->getMatrix();
+
+    EXPECT_EQ(*extractedThirdMatrix, *cMatrixContent);
+
 }
